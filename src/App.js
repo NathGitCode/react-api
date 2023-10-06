30409b4f08c3dd0d224710728c8c26f77534c5eb
Nathan Stringer <nathstringer@gmail.com>
2023-10-06 15:43:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [chars, setChars] = useState([]);
+  useEffect(() => {
+    const fetchHarryPotter = async () => {
+      const response = await fetch(
+        `https://hp-api.onrender.com/api/characters`
+      );
+      const data = await response.json();
+      setChars(data);
+    };
+    fetchHarryPotter();
+  }, []);
+
+  const [actor, setActor] = useState([]);
+  const pushData = (items, i) => {
+    let storedActor = [...actor];
+    storedActor.splice(i, 1);
+    storedActor.push(items);
+    setActor(storedActor);
+  };
+  console.log(actor);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flexContainer">
+        {chars.map((items, index) => {
+          return (
+            <div key={index}>
+              <button
+                className="actorContainer"
+                onClick={() => pushData(items)}
+              >
+                {items.actor}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="infoContainer">
+        <h3>Actor Information</h3>
+        {actor.map((actors, i) => {
+          return (
+            <div key={i}>
+              <h4>{actors.name}</h4>
+              <p>Species : {actors.species}</p>
+              <p>House : {actors.house}</p>
+              <p>Gender : {actors.gender}</p>
+              <img src={actors.image} alt="actor" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
